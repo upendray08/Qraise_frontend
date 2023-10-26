b225699d6dd4a79b538346df2f2252903796da47
Nitin Kumar Dubey <80614507+nitinkumardubey@users.noreply.github.com>
2023-10-26 13:17:59 +0530

    added forget password page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Index from './components/Index';
 import { Routes, Route } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
+import Forgot from './components/Forgotpass';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
                     <Route path='/' element={<Index />} />
                     <Route path='/register' element={<Register />} />
                     <Route path='/login' element={<Login />} />
+                    <Route path='/forget_pass' element={<Forgot />} />
                     <Route path='/index' element={<Index />} />
                </Routes>
           </>
--- src/components/Forgotpass.js
+++ src/components/Forgotpass.js
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom'
 
 const stylee = {
      width: "40%",
-     height: "460px",
+     height: "300px",
      margin: "0px auto",
      backgroundColor: "rgba(176,176,176,0.7)",
      backdropFilter: "blur(1px)",
@@ -13,7 +13,7 @@ const stylee = {
      boxShadow: " 0px 0px 10px 2px rgb(101 78 138)",
      padding: '30px'
 }
-const Login = () => {
+const Forgotpass = () => {
      return (
           <>
                <div>
@@ -23,25 +23,25 @@ const Login = () => {
                     <div class="page" style={stylee}>
                          <div class="register">
                               <br />
-                              <h2 style={{ textAlign: 'center', fontWeight: '500' }}>Login</h2>
+                              <h2 style={{ textAlign: 'center', fontWeight: '500' }}>Forget Password</h2>
                               <br />
-                              <h6 style={{ textAlign: 'center', fontWeight: '500' }}>Already have an account.</h6>
+                              {/* <h6 style={{ textAlign: 'center', fontWeight: '500' }}>Already have an account.</h6> */}
                               <form>
                                    <div class="mb-3">
                                         <label for="email" class="form-label">Email address</label>
                                         <input type="email" class="form-control" id="email" aria-describedby="emailHelp" />
                                    </div>
-                                   <div class="mb-3">
+                                   {/* <div class="mb-3">
                                         <label for="password" class="form-label">Password</label>
                                         <input type="password" class="form-control" id="password" />
                                    </div>
                                    <div class="mb-3" style={{ display: 'flex' }}>
-                                        <NavLink to="/forget_pass" style={{ marginLeft: 'auto', color: 'black ' }}>Forgot Password?</NavLink>
+                                        <NavLink to="/forgot_pass" style={{ marginLeft: 'auto', color: 'black ' }}>Forgot Password?</NavLink>
                                    </div>
-                                   <br />
+                                   <br /> */}
                                    <div class="mb-3" style={{ flexDirection: 'row-reverse', display: 'flex' }}>
                                         <button type="submit" class="btn btn-primary">Cancel</button>
-                                        <button type="submit" style={{ marginRight: '5px' }} class="btn btn-primary">Login</button>
+                                        <button type="submit" style={{ marginRight: '5px' }} class="btn btn-primary">Submit</button>
                                    </div>
                               </form>
                          </div>
@@ -51,4 +51,4 @@ const Login = () => {
      )
 }
 
-export default Login
+export default Forgotpass
