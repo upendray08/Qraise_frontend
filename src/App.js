6a686e1a28d73a210b8363a0c89d9552125b38a1
Upendra Yadav <yupendra2000@gmail.com>
2023-10-27 13:01:13 +0530

    changes in App.js and index.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import React , {useState} from "react";
 import Index from "./components/Index";
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Forgot from "./components/Forgotpass";
-import React, { useState } from "react";
+import ViewCampaign from "./components/ViewCampaign";
+import CreateCampaign from "./components/CreateCampaign";
+import Notification from "./components/Notification";
+import UserProfile from "./components/UserProfile";
 import CampaignPage from "./components/CampaignPage";
 import FormPage from "./components/CampaignForm";
 
@@ -17,6 +21,14 @@ function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forget_pass" element={<Forgot />} />
+        <Route path="/view_campaign" element={<ViewCampaign />} />
+        <Route path="/create_campaign" element={<CreateCampaign />} />
+        <Route path="/notification" element={<Notification />} />
+        <Route path="/profile" element={<UserProfile />} />
         <Route path="/" element={<Index />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
@@ -39,4 +51,5 @@ function App() {
     </>
   );
 }
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import '@fortawesome/fontawesome-free/css/all.css';
+// import '@fortawesome/fontawesome-free/css/all.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
