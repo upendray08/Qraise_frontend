2550fc656aca0756243cb6b452728dfa821f9685
Upendra Yadav <yupendra2000@gmail.com>
2023-10-27 12:33:35 +0530

    changes in App.js file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./App.css";
 import Index from "./components/Index";
 import { Routes, Route } from "react-router-dom";
@@ -39,34 +38,5 @@ function App() {
       </Routes>
     </>
   );
-=======
-import './App.css';
-import Index from './components/Index';
-import { Routes, Route } from 'react-router-dom';
-import Register from './components/Register';
-import Login from './components/Login';
-import Forgot from './components/Forgotpass';
-import ViewCampaign from './components/ViewCampaign';
-import CreateCampaign from './components/CreateCampaign';
-import Notification from './components/Notification';
-import UserProfile from './components/UserProfile';
-
-function App() {
-     return (
-          <>
-               <Routes>
-                    <Route path='/' element={<Index />} />
-                    <Route path='/register' element={<Register />} />
-                    <Route path='/login' element={<Login />} />
-                    <Route path='/forget_pass' element={<Forgot />} />
-                    <Route path='/view_campaign' element={<ViewCampaign />} />
-                    <Route path='/create_campaign' element={<CreateCampaign />} />
-                    <Route path='/notification' element={<Notification />} />
-                    <Route path='/profile' element={<UserProfile />} />
-               </Routes>
-          </>
-     );
->>>>>>> 0256439d84b9a5b41aa1c33af17a135e33934d07
 }
-
 export default App;
